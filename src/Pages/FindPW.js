489e56985cd6e7e2fb88bbe27c791c8deeafe4fc
Cho-Seung-Hyeon <jodu334@gmail.com>
2022-09-03 13:04:38 +0900

    [FEAT]비밀번호 초기화

--- src/Pages/FindPW.js
+++ src/Pages/FindPW.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Modal, Button, Form, Container} from 'react-bootstrap'
+import axios from 'axios';
+
 
 function FindPW(props) {
+
+  let [name, setName] =useState('');
+  let [email, setEmail] = useState('');
+
+  let [FindpwButton, setFindpwButton] = useState(true);
+
+  function changeButton() {
+    name.length>=1 && email.includes('@') ?  setFindpwButton(false) : setFindpwButton(true);
+   }
+
+   const Find = () => {
+    axios
+      .post('http://203.250.32.29:2201/api/manager-service/ManagerPasswordReset', {
+        managerName: name,
+        managerEmail: email,
+      })
+      .then(response => {
+        // Handle success.
+        console.log('성공');
+        console.log(response.data)
+        alert('회원님의 이메일로 임시 비밀번호가 발송되었습니다.');
+        
+      })
+      .catch(error => {
+        // Handle error.
+        console.log('로그인 실패', error.response);
+        alert(error.response.data);
+      });
+   }
   
   return (
     <Modal
@@ -19,18 +50,22 @@ function FindPW(props) {
       <Modal.Body>
       <Form>
         <Form.Group className="mb-3">
-          <Form.Label>아이디</Form.Label>
-          <Form.Control type="text" placeholder="아이디를 입력하세요" />
+          <Form.Label>이름</Form.Label>
+          <Form.Control type="text" placeholder="홍길동" value={name} onChange={(e)=> {
+                 setName(e.target.value);
+              }}onKeyUp={changeButton} />
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>이메일</Form.Label>
-          <Form.Control type="email" placeholder="12345@abc.com" />
+          <Form.Control type="email" placeholder="12345@abc.com" value={email} onChange={(e)=> {
+                 setEmail(e.target.value);
+              }}onKeyUp={changeButton}/>
         </Form.Group>
       </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" type="button">
+        <Button variant="primary" type="button" disabled={FindpwButton} onClick={()=> {Find()}}>
             비밀번호찾기
         </Button>
       </Modal.Footer>
